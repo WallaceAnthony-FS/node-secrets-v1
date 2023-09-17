3f4e021844a65a53b85b0da90bc635dae1f2ed35
Anthony Wallace <anthonyt.wallace@icloud.com>
2023-09-17 09:35:49 -0700

    v1

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const dir = __dirname;
+const filenames = fs.readdirSync(dir);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  let files = filenames
+    .map((filename) => {
+      return `${filename}\n`;
+    })
+    .join("");
+
+  res.end(files);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
